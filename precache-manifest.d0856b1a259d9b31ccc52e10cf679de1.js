9f245d81e615d8fd9641cec22dcc443ff36bb856
VladDlogush <vlad.dlogush21@gmail.com>
2019-10-05 13:37:48 +0300

    Updates

--- precache-manifest.d0856b1a259d9b31ccc52e10cf679de1.js
+++ precache-manifest.d0856b1a259d9b31ccc52e10cf679de1.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d059d83820e4a20279b4e7abffb108be",
+    "revision": "3d07c1d1c44d42ef1545344b7fc6737c",
     "url": "/goit-react-hw-02-reader/index.html"
   },
   {
-    "revision": "42f93780a08c06709917",
-    "url": "/goit-react-hw-02-reader/static/css/main.0f2fa4f7.chunk.css"
+    "revision": "82c5bbfa106f2c165490",
+    "url": "/goit-react-hw-02-reader/static/css/main.b0150247.chunk.css"
   },
   {
     "revision": "022ab5de451fd457ba7c",
     "url": "/goit-react-hw-02-reader/static/js/2.23358ce4.chunk.js"
   },
   {
-    "revision": "42f93780a08c06709917",
-    "url": "/goit-react-hw-02-reader/static/js/main.3daf9536.chunk.js"
+    "revision": "82c5bbfa106f2c165490",
+    "url": "/goit-react-hw-02-reader/static/js/main.0d69296c.chunk.js"
   },
   {
     "revision": "263de4f3092350e73728",
